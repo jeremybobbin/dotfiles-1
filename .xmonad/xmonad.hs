e397724bd3e970c99f3f20f5ffb53b834f598fba
Stephen A. Balaban <s@lambdal.com>
2014-04-04 19:43:51 +0800

    Working xmonad conf

--- .xmonad/xmonad.hs
+++ .xmonad/xmonad.hs
@@ -1,5 +1,9 @@
 import XMonad
 import XMonad.Hooks.Script
+import XMonad.Util.Run(spawnPipe)
+import XMonad.Util.EZConfig
+
+myModMask = mod1Mask
 
 main = do
     xmonad $ defaultConfig
@@ -7,6 +11,9 @@ main = do
       , borderWidth   = mBorder
       , startupHook   = execScriptHook mStartupHook
       }
+      `additionalKeys`
+      [ ((myModMask .|. shiftMask, xK_b), spawn "chromium-browser &> /dev/null &")
+      ]
 mTerm = "gnome-terminal"
 mBorder = 3
 mStartupHook = "xmonad"
