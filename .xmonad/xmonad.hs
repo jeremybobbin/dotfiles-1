7ebc45952288f2b215b836f036800c3e85d63a33
Stephen A. Balaban <s@lambdal.com>
2014-04-07 00:23:44 -0700

    Added mac keybindings to xmonad.

--- .xmonad/xmonad.hs
+++ .xmonad/xmonad.hs
@@ -13,6 +13,12 @@ main = do
       }
       `additionalKeys`
       [ ((myModMask .|. shiftMask, xK_b), spawn "chromium-browser &> /dev/null &")
+      , ((0, 0x1008ff03), spawn "sudo macbook.sh mon-dec")
+      , ((0, 0x1008ff02), spawn "sudo macbook.sh mon-inc")
+      , ((0, 0x1008ff06), spawn "sudo macbook.sh kbd-dec")
+      , ((0, 0x1008ff05), spawn "sudo macbook.sh kbd-inc")
+      , ((0, 0x1008ff11), spawn "sudo macbook.sh vol-dec")
+      , ((0, 0x1008ff13), spawn "sudo macbook.sh vol-inc")
       ]
 mTerm = "gnome-terminal"
 mBorder = 3
---
+++ scripts/macbook.sh
@@ -0,0 +1,53 @@
+#!/bin/bash
+MAX_KBD=255
+MAX_MON=1808
+MAX_VOL=100
+MIN_ALL=0
+INCREMENT=1
+BACKLIGHT_CUR=/sys/class/backlight/intel_backlight/actual_brightness
+BACKLIGHT_VAL=/sys/class/backlight/intel_backlight/brightness
+KBD_CUR=/sys/class/leds/smc\:\:kbd_backlight/brightness
+KBD_VAL=/sys/class/leds/smc\:\:kbd_backlight/brightness
+
+cmd=$1
+
+max() {
+    echo $1 $2 | awk '{ if ($1 > $2) { print $1 } else { print $2 } }'
+}
+
+min() {
+    echo $1 $2 | awk '{ if ($1 < $2) { print $1 } else { print $2 } }'
+}
+
+floor0() {
+    max 0 $1
+}
+
+ceil() {
+    min $1 $2
+}
+
+if ! [[ $UID == 0 ]]
+then
+    echo 1>&2 requires root access
+    exit 1
+fi
+
+case "$cmd" in
+    mon-dec)
+        CURRENT=$(cat $BACKLIGHT_CUR)
+        max $MIN_ALL $(($CURRENT - $INCREMENT)) > $BACKLIGHT_VAL
+        ;;
+    mon-inc)
+        CURRENT=$(cat $BACKLIGHT_CUR)
+        min $MAX_MON $(($CURRENT + $INCREMENT)) > $BACKLIGHT_VAL
+        ;;
+    kbd-dec)
+        CURRENT=$(cat $KBD_CUR)
+        max $MIN_ALL $(($CURRENT - $INCREMENT)) > $KBD_VAL
+        ;;
+    kbd-inc)
+        CURRENT=$(cat $KBD_CUR)
+        min $MAX_KBD $(($CURRENT + $INCREMENT)) > $KBD_VAL
+        ;;
+esac
