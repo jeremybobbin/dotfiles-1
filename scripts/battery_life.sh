0b9b5a7395cc2b2f49a6cb1eb8cb23e54d4679b6
Jeremy Bobbin <jeremybobbin@gmail.com>
2019-08-31 11:17:12 -0700

    bc -> dc; Python rounding -> printf

--- scripts/battery_life.sh
+++ scripts/battery_life.sh
@@ -5,11 +5,16 @@ else
     ROUND_TO=3; 
 fi
 BATTERY=/sys/class/power_supply/BAT0
+
+
 power_remaining() {
-    echo "`cat $BATTERY/energy_now` / `cat $BATTERY/energy_full`" | bc -l
+	echo "16 k `cat $BATTERY/energy_{now,full}` / p" | dc
 }
+
 round_to() {
-    python -c "print(round($1, $2))"
+	printf "%1.$2f" $1
 }
+
 power_rounded=$(round_to $(power_remaining) $ROUND_TO)
+
 echo $power_rounded
